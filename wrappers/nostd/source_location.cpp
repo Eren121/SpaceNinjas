912eaa3e5422c522c24e3238fb8769a85b64589a
Raphael <frantze@raphael.gmail.com>
2021-10-16 02:32:04 +0200

    fix msvc compilation error (forward declare C struct cause troubles)

--- wrappers/nostd/source_location.cpp
+++
@@ -1,9 +0,0 @@
-#include "source_location.hpp"
-
-std::ostream& operator<<(std::ostream& lhs, const nostd::source_location& rhs)
-{
-    lhs << rhs.file_name() << "(" << rhs.line() << ":" << rhs.column() << ")";
-    lhs << " `" << rhs.function_name() << "`";
-
-    return lhs;
-}
